11f480f924eb58b09059f066532a131ce915d06f
anaes0 <anaesvg@gmail.com>
2022-10-25 14:05:03 +0100

    Blog module: testing

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,21 +1,24 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 //import Mocktest from "./Mocktest";
 import Blog from "../views/Blog";
 import Contact from "../views/Contact";
-
-import { BrowserRouter as Router } from "react-router-dom";
 import App from "../App";
 import Button from "../components/Background/Button";
 
+//source: https://reactjs.org/docs/testing-recipes.html
+
 // * INITIAL TESTING *
 /*Test 1 - Test render landing page*/
+
 test("renders the landing page", () => {
   render(<App />);
 });
 
 /*Test 2 - Test Button.js (-->Background) component and if a button exists*/
+
 test("renders button component", () => {
   render(
     <Router>
@@ -27,6 +30,7 @@ test("renders button component", () => {
 });
 
 /*Test 3 - Test render blog page */
+
 test("renders blog page", () => {
   render(
     <Router>
@@ -36,6 +40,7 @@ test("renders blog page", () => {
 });
 
 /*Test 4 - Test render contact page*/
+
 test("renders contact page", () => {
   render(
     <Router>
--- src/test/BlogModule.test.js
+++ src/test/BlogModule.test.js
@@ -2,4 +2,20 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import BlogNavBar from "../components/Blog/BlogNavBar/BlogNavBar";
+
+/* sources: https://www.npmjs.com/package/@testing-library/jest-dom
+            jestjs.io/docs/expect 
+            https://testing-library.com/docs/react-testing-library/cheatsheet/ */
+
 // * BLOG MODULE TESTING *
+/*Test 1 - Test render blog navbar elements on screen (user not logged)*/
+test("renders blog navbar elements (user not logged)", () => {
+  render(
+    <Router>
+      <BlogNavBar />
+    </Router>
+  );
+  expect(screen.getByText(/blog/i)).toBeInTheDocument();
+  expect(screen.getByText(/log in/i)).toBeInTheDocument();
+});
