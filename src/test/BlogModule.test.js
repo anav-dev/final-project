81eed5c9189f97b9cf5fbc7089b2d8aa23451fbf
anaes0 <anaesvg@gmail.com>
2022-10-25 14:45:31 +0100

    Blog module: testing

--- src/test/BlogModule.test.js
+++ src/test/BlogModule.test.js
@@ -6,7 +6,9 @@ import BlogNavBar from "../components/Blog/BlogNavBar/BlogNavBar";
 
 /* sources: https://www.npmjs.com/package/@testing-library/jest-dom
             jestjs.io/docs/expect 
-            https://testing-library.com/docs/react-testing-library/cheatsheet/ */
+            https://www.w3.org/TR/wai-aria/#role_definitions
+            https://testing-library.com/docs/react-testing-library/cheatsheet/ 
+            https://www.freecodecamp.org/news/8-simple-steps-to-start-testing-react-apps-using-react-testing-library-and-jest/*/
 
 // * BLOG MODULE TESTING *
 /*Test 1 - Test render blog navbar elements on screen (user not logged)*/
@@ -16,6 +18,26 @@ test("renders blog navbar elements (user not logged)", () => {
       <BlogNavBar />
     </Router>
   );
-  expect(screen.getByText(/blog/i)).toBeInTheDocument();
-  expect(screen.getByText(/log in/i)).toBeInTheDocument();
+
+  const loginLink = screen.getByRole("link", { name: "Log in" });
+  const blogLink = screen.getByRole("link", { name: "Blog" });
+  expect(loginLink).toBeInTheDocument();
+  expect(blogLink).toBeInTheDocument();
+
+  //expect(screen.getByText(/blog/i)).toBeInTheDocument();
+  //expect(screen.getByText(/log in/i)).toBeInTheDocument();
 });
+
+/*Test 2 - Test render blog navbar element: "Create Post" is NOT on screen (user not logged)- 
+This test should fail 
+test("renders blog navbar elements:createpost (user not logged)", () => {
+  render(
+    <Router>
+      <BlogNavBar />
+    </Router>
+  );
+  const createpostLink = screen.getByRole("link", { name: "Create post" });
+  expect(createpostLink).toBeInTheDocument();
+  //expect(screen.getByText(/create post/i)).toBeInTheDocument();
+});
+*/
