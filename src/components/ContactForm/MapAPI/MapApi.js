c7df453a232ac32944b7cdaade550ed4414cf819
anaes0 <anaesvg@gmail.com>
2022-10-27 15:59:52 +0100

    MapAPI component added

---
+++ src/components/ContactForm/MapAPI/MapApi.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MapStyle from "./MapApi.css";
+
+function MapApi() {
+  return (
+    <div className="map-container">
+      <div className="map-title">GOOGLE MAP HERE</div>
+    </div>
+  );
+}
+
+export default MapApi;
