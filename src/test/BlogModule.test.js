134013206966ceb11a343e06e4a81669a18c12b7
anaes0 <anaesvg@gmail.com>
2022-10-31 14:47:00 +0000

    Blog module testing

--- src/test/BlogModule.test.js
+++ src/test/BlogModule.test.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import BlogNavBar from "../components/Blog/BlogNavBar/BlogNavBar";
-import Blog, { isAuth } from "../views/Blog";
+import Blog from "../views/Blog";
 import Login from "../components/Blog/PostsList/Login/Login";
+
+import { waitForElementToBeRemoved, prettyDOM } from "@testing-library/react";
 
 /* sources: https://testing-library.com/docs/queries/about/
             https://www.w3.org/TR/wai-aria/#role_definitions
@@ -29,8 +31,9 @@ test("renders blog navbar elements (user not logged)", () => {
   expect(blogLink).toBeInTheDocument(); //expect(screen.getByText(/log in/i)).toBeInTheDocument();
 });
 
-/* Test 2 - Test render blog navbar element: "Create Post" is NOT on screen (user not logged)- 
-This test should fail 
+/* Test 2 - Test render blog navbar element: "Create Post" is NOT on screen (user not logged) This test should fail */
+
+/*
 test("renders blog navbar elements:createpost (user not logged)", () => {
   render(
     <Router>
@@ -39,46 +42,51 @@ test("renders blog navbar elements:createpost (user not logged)", () => {
   );
   const createpostLink = screen.getByRole("link", { name: "Create post" });
   expect(createpostLink).toBeInTheDocument(); //expect(screen.getByText(/create post/i)).toBeInTheDocument();
-  
 });
 */
 
-// * Test 3 - ?????? Test userAuth const is false when user is not logged
-/*
-test("check isAuth is false (user not logged)", () => {
+/* 2. Logging process 
+STEPS:
+a) Check page loads
+b) Check elements displayed (user not logged)
+c) Loggin process
+d) Check elements displayed (user logged)
+*/
+
+// * Test 3- Test elements displayed when Sign in with Google Button is clicked
+test("After log in btn clicked, some elements are displayed and routing is correct", async () => {
   render(
     <Router>
       <Blog />
     </Router>
   );
+  //console.log("Loging page (not logged)\n:" + prettyDOM(document));
+  //TODO: Check that the elements are correct as not logged
 
-  expect(isAuth).toBe(false);
-});
-*/
-// 2. Logging process
-
-// * Test 4- Test firebase auth's signInWithPopup method when Sign in with Google Button is clicked
-test("Firebase auth triggerd when login clicked", () => {
+  // Login process
+  const loginBtn = screen.getByRole("link", { name: "Log in" });
+  fireEvent.click(loginBtn);
+  expect(window.location.pathname).toBe("/blog/login");
+  console.log("Path document after click: " + window.location);
+  // The path is ok, render loging page before checking
   render(
     <Router>
       <Login />
     </Router>
   );
-  const googleBtn = screen.getByRole("button", {
+  const gogSignText = screen.getByRole("button", {
     name: "Sign in with Google",
   });
 
-  fireEvent.click(googleBtn);
+  expect(gogSignText).toBeInTheDocument();
+  console.log("Loging page (after click)\n:" + prettyDOM(document));
 
-  //fireEvent.click();
-  // --> Error: "Jest worker encountered 4 child process exceptions, exceeding retry limit"
+  return;
 
-  //await waitForElementToBeRemoved(() =>
-  //screen.getByText(/Sign In With Google/i)
-  //);
   //expect(screen.getByText(/Create post/i)).toBeInTheDocument();
 });
 
 // 3. Main parts on website (logged)
 // 4. Create post page (website content)
+
 // 5. Create post, check on the database, check it on the website and delete in database, and check delete.
